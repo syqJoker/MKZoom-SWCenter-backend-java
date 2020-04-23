a9b1b87be5c3c5573994195cd29c204d1c6014f2
大圣 <syqenter@live.com>
2020-04-23 22:12:58 +0800

    init started

---
+++ src/main/java/com/mksun/swcenter/SwcenterApplication.java
@@ -0,0 +1,13 @@
+package com.mksun.swcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mksun/swcenter/SwcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mksun.swcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
